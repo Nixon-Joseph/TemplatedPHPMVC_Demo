179d7f9e196e64c3f9deeef8c35964b3d3104ba7
Joseph Nixon <nieminen432@gmail.com>
2021-03-29 13:23:02 -0500

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,87 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'devpires/templated-php-mvc-demo',
+  ),
+  'versions' => 
+  array (
+    'bshaffer/oauth2-server-php' => 
+    array (
+      'pretty_version' => 'v1.11.1',
+      'version' => '1.11.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a0c8000d4763b276919e2106f54eddda6bc50fa',
+    ),
+    'devpirates/templated-php-mvc' => 
+    array (
+      'pretty_version' => '0.1.1',
+      'version' => '0.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a1849241b9103d5d1bf6ec85d19709d15d15bdca',
+    ),
+    'devpires/templated-php-mvc-demo' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'liquid/liquid' => 
+    array (
+      'pretty_version' => '1.4.22',
+      'version' => '1.4.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd3e9c3de5b8a17840af2a29ac39cfe3afac55282',
+    ),
+    'netresearch/jsonmapper' => 
+    array (
+      'pretty_version' => 'v4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8bbc021a8edb2e4a7ea2f8ad4fa9ec9dce2fcb8d',
+    ),
+    'pelago/emogrifier' => 
+    array (
+      'pretty_version' => 'v5.0.0',
+      'version' => '5.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b43b650880d189b0ada61d95d0729c7424b1752d',
+    ),
+    'sonata-project/google-authenticator' => 
+    array (
+      'pretty_version' => '2.3.1',
+      'version' => '2.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '71a4189228f93a9662574dc8c65e77ef55061b59',
+    ),
+    'symfony/css-selector' => 
+    array (
+      'pretty_version' => 'v5.2.4',
+      'version' => '5.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f65f217b3314504a1ec99c2d6ef69016bb13490f',
+    ),
+  ),
+);
